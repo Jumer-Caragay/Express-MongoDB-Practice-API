74710146456ab04833b29a91d99d6b9c2271b167
Jumer Ryan Caragay <jumer.caragay@gmail.com>
2018-09-06 14:35:25 -0700

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -27,6 +28,30 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+app.get('/todos/:id', (req, res) => {
+  let id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send({});
+  }
+  // validate id
+    // 404 - send back empty
+
+  Todo.findById(id).then((todo) => {
+    if(!todo) {
+      return res.status(404).send({});
+    }
+
+    res.send({todo});
+  }).catch((e) => res.status(400).send({}));
+  //findById
+    //success
+      // if todo - send it back
+      // if no todo - send back 404 with empty body
+    //error
+      // 400 and send empty body back
 });
 
 app.listen(3000, () => {
