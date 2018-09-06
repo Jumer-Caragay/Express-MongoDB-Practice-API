1954ef6c2747d355e2af6d7331690d0615d7faa6
Jumer Ryan Caragay <jumer.caragay@gmail.com>
2018-09-06 13:06:24 -0700

    Added POST todo route and refactor mongoose

---
+++ server/models/todo.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+let Todo = mongoose.model('Todo', {
+  text: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true // trim the string first and then validate the length > 1
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  completedAt: {
+    type: Number,
+    default: null
+  }
+});
+
+module.exports = {Todo};
+
+// let otherTodo = new Todo({
+//   text: 'Something to do'
+// });
+
+// otherTodo.save().then((doc) => {
+//   console.log(JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//   console.log('Unable to save Todo', e)
+// });
