22e4a1e4b9bdf9af64e89a6c334df7ed7b89f3e1
Jumer Ryan Caragay <jumer.caragay@gmail.com>
2018-09-06 13:56:07 -0700

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,16 @@ const request = require('supertest');
 const {app} = require('../server');
 const {Todo} = require('../models/todo');
 
+const todos = [{
+  text: 'First test todo'
+}, {
+  text: 'Second test todo'
+}];
+
 beforeEach((done) => {
-  Todo.remove({}).then(() => done());
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -23,7 +31,7 @@ describe('POST /todos', () => {
         return done(err);
       }
 
-      Todo.find().then((todos) => {
+      Todo.find({text}).then((todos) => {
         expect(todos.length).toBe(1) // checks to add 1 to database
         expect(todos[0].text).toBe(text); // checks if the todo was the todo we added into the database
         done();
@@ -43,9 +51,21 @@ describe('POST /todos', () => {
       }
 
       Todo.find().then((todos) => {
-        expect(todos.length).toBe(0)
+        expect(todos.length).toBe(2)
         done();
       }).catch((e) => done(e)); //catch any errors if errors within database errors
     });
+  });
+});
+
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app)
+    .get('/todos')
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todos.length).toBe(2) // test the number of things in the database should be 2
+    })
+    .end(done);
   });
 });
