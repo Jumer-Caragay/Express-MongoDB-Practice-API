b6d1b5bbdb7d9b712df372665ba6057abf936c6c
Jumer Ryan Caragay <jumer.caragay@gmail.com>
2018-09-07 17:37:18 -0700

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -54,6 +54,31 @@ app.get('/todos/:id', (req, res) => {
       // 400 and send empty body back
 });
 
+app.delete('/todos/:id', (req, res) => {
+  // get the id
+  let id = req.params.id;
+  
+  // validate the id -> not valid? return 404
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  // remove todo by id
+  Todo.findByIdAndRemove(id).then((doc) => {
+    if (!doc) {
+      return res.status(404).send();
+    }
+
+    res.status(200).send(doc);
+  }).catch((e) => res.status(400).send());
+    // success
+      // check if document came back
+      // if no doc, send 404
+      // if doc send doc with 200 status code
+    // error
+      // 400 with empty body
+});
+
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
 });
