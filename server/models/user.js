f0f059e44aba2425bb3c321eacf03ecd2f758e8c
Jumer Ryan Caragay <jumer.caragay@gmail.com>
2018-09-10 05:22:25 -0700

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -69,6 +69,26 @@ UserSchema.statics.findByToken = function (token) {
   }); // return the promise so we can tact the .then inside server.js
 };
 
+UserSchema.statics.findByCredentials = function (email, password) {
+  let User = this;
+
+  return User.findOne({email}).then((user) => {
+    if(!user) {
+      return Promise.reject(); // fire the catch call in server.js
+    }
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password,(err, res) => {
+        if(res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
+  });
+};
+
 UserSchema.pre('save', function(next) {
   const user = this;
 
